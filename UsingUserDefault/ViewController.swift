984b82d660a076b23a414b5c134fd063ff10fe39
sefacr <aacarsefa@gmail.com>
2024-05-15 16:04:03 +0300

    a simple user default using app

--- UsingUserDefault/ViewController.swift
+++ UsingUserDefault/ViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let ud = UserDefaults.standard //initilazion ettik
+        
+        
+        //Kayıt
+        ud.set("Ahmet", forKey: "name")
+        ud.setValue(1.78, forKey: "boy")
+        
+        //silme
+        //ud.removeObject(forKey: "name")
+        
+        
+        //okuma
+        let receivedName = ud.string(forKey: "name") ?? "empty"
+        let receivedHeight = ud.double(forKey: "boy")
+        print("Gelen Ad: \(receivedName)")
+        print("Gelen Boy: \(receivedHeight)")
+        
+        
+        
+        //Sayac Uygulama
+        var count = ud.integer(forKey: "count")
+        count += 1
+        ud.setValue(count, forKey: "count")
+        counterLbl.text = "Opening Count: \(count)"
     }
 
 
